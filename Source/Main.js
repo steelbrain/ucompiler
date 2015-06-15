f9b1bacc441ba3d60132355fd842c5317202252d
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-15 05:28:26 -0600

    :new: Make compileStream work

--- Source/Main.js
+++ Source/Main.js
@@ -2,11 +2,15 @@
 let NodeStream = require('stream')
 let FS = require('fs')
 let Helpers = require('./Helpers')
+let Path = require('path')
 class UCompiler{
-  static compileFile(Path, Options){
-    return UCompiler.compileStream(FS.createReadStream(Path), Options)
+  static compileFile(FilePath, Options){
+    Options = Helpers.normalizeOptions(Options)
+    Options.File = {Path: FilePath, Extension: Path.extname(FilePath)}
+    return UCompiler.compileStream(FS.createReadStream(FilePath), Options)
   }
   static compileString(Content, Options){
+    Options = Helpers.normalizeOptions(Options)
     let Stream = new NodeStream.Readable()
     Stream._read = function(){ }
     Stream.push(Content)
@@ -15,7 +19,36 @@ class UCompiler{
   }
   static compileStream(Stream, Options){
     Options = Helpers.normalizeOptions(Options)
-    console.log(Stream)
+    return new Promise(function(Resolve){
+      let Valid = []
+      for(let Plugin of UCompiler.Plugins){
+        if(Plugin.Validate(Options)){
+          Valid.push(Plugin)
+        }
+      }
+      Resolve(Promise.all(Valid.sort(function(a, b){
+          return a.Info.Priority - b.Info.Priority
+        }).map(function(Plugin){
+          return Plugin.Stream(Options)
+        })))
+    }).then(function(Streams){
+        for(let _Stream of Streams){
+          Stream = Stream.pipe(_Stream)
+        }
+        return Stream
+      })
   }
 }
+
+UCompiler.Plugins = new Set()
+
+// Loading Plugins
+require('../Plugins/Javascript').Register(UCompiler)
+
+UCompiler.compileFile('/var/web/PublishSpace/dQuery/Source/Main.js').then(function(Stream){
+   Stream.pipe(process.stdout)
+}).catch(function(err){
+  console.error(err)
+})
+
 module.exports = UCompiler
