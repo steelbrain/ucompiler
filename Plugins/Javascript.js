38a3f61e8fc9e82bf5580ac569c797cf2ca94c32
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-15 05:42:00 -0600

    :new: Add more Javascript tags

--- Plugins/Javascript.js
+++ Plugins/Javascript.js
@@ -1,9 +1,16 @@
 "use strict"
 let Plugin = require('../Source/Plugin')
+let Path = require('path')
 class PluginJS extends Plugin{
   static Process(Contents, Options){
-    return Contents
+    return this.ProcessTags('//', Contents, Options)
   }
 }
 PluginJS.Info.Extensions = ['.js']
+PluginJS.Tags.set('Compiler-Compress', function(Name, Value, _, Options){
+  Options.Compress = Value === 'true'
+})
+PluginJS.Tags.set('Compiler-Transpile', function(Name, Value, _, Options){
+  Options.Transpile = Value === 'true'
+})
 module.exports = PluginJS
