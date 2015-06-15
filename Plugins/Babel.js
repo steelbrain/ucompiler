b97f829d522e327979f6dcbf71edf8fda76ffb1e
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-15 07:13:36 -0600

    :new: Add Babel Plugin

---
+++ Plugins/Babel.js
@@ -0,0 +1,17 @@
+"use strict"
+let Plugin = require('../Source/Plugin')
+let Path = require('path')
+let BabelJS
+class Babel extends Plugin{
+  static Process(Contents, Options){
+    if(!Options.Transpile) return Contents
+    BabelJS = BabelJS || require('babel-core')
+    return BabelJS.transform(Contents, {
+      filenameRelative: Options.File.Name,
+      sourceRoot: Options.File.Dir
+    }).code
+  }
+}
+Babel.Info.Priority = 50
+Babel.Info.Extensions = ['.js']
+module.exports = Babel
