c4f2989a0f8f1970d276d4690aec99aed57cdbcd
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-15 07:35:36 -0600

    :new: Add CLI Compile.js

---
+++ CLI/Compile.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+"use strict"
+let Main = require('../Source/Main')
+let Minimist = require('minimist')
+let Options = Minimist(process.argv.slice(2))
+let FS = require('fs')
+try {
+  if(!Options['_'].length) throw new Error("Please specify a file to compile")
+  Main.compileFile(Options['_'][0], Options).then(function(Output) {
+    if(Options.Output){
+      FS.writeFileSync(Options.Output, Output)
+    } else {
+      process.stdout.write(Output)
+    }
+  }).catch(function(err){
+    console.log(err)
+  })
+} catch(err){
+  console.log(err)
+}
