6e9848c16cff5060ed85c39ca3c096e5af95e5c8
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-29 19:20:22 -0600

    :new: Add Browserify plugin

---
+++ Plugins/Browserify.js
@@ -0,0 +1,35 @@
+"use strict"
+let Plugin = require('../Source/Plugin')
+let Path = require('path')
+let Stream = require('stream')
+let Browserify
+class BrowserifyJS extends Plugin{
+  static Process(Contents, Options){
+    if(!Options.Browserify) return Contents
+    Browserify = Browserify || require('browserify')
+    let Readable = new Stream.Readable()
+    Readable._read = function(){
+      Readable.push(Contents)
+      Readable.push(null)
+    }
+    let Browserified = Browserify(Readable, {
+      basedir: Options.File.Dir,
+      debug: Options.SourceMap,
+      fullPaths: false,
+      filename: Options.File.Path
+    })
+    let Deferred = Promise.defer()
+    Browserified.bundle(function(err, buffer){
+      if(err) return Deferred.reject(err)
+      if(Options.SourceMap){
+        Options.Content.SourceMap = null
+      }
+      Deferred.resolve(buffer)
+    })
+    return Deferred.promise
+  }
+}
+BrowserifyJS.Initialize()
+BrowserifyJS.Info.Priority = 150
+BrowserifyJS.Info.Extensions = ['.js', '.coffee']
+module.exports = BrowserifyJS
