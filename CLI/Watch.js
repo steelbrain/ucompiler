2a715da2fa409b218b50309b126aaa723132eb75
steelbrain <aneesiqbalbhatti@gmail.com>
2015-07-02 05:50:16 -0600

    :new: Add watcher

---
+++ CLI/Watch.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+"use strict"
+let Main = require('../Source/Main')
+let Minimist = require('minimist')
+let FSWatcher = require('node-fswatcher')
+let Options = Minimist(process.argv.slice(2))
+let FS = require('fs')
+try {
+  if(!Options['_'].length) throw new Error("Please specify a file/directory to watch")
+  let Watcher = new FSWatcher
+  Watcher.watch(Options['_'][0])
+  Watcher.on('update', function(Info){
+    FS.access(Info.Path, FS.R_OK, function(err){
+      if(!err)
+        Main.compileFile(Info.Path, Options).then(function(Output) {
+          if(Options.Output){
+            FS.writeFileSync(Options.Output, Output)
+          }
+        }).catch(function(err){
+          console.log(err)
+        })
+    })
+  })
+} catch(err){
+  console.log(err)
+}
