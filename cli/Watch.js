0c912f0df7b63c89883fb7bd4990bc1f00a99025
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-15 19:06:18 -0600

    :new: Use the new watcher API in uc-watch

--- cli/Watch.js
+++ cli/Watch.js
@@ -1,27 +1,15 @@
 #!/usr/bin/env node
 "use strict"
-let Main = require('../lib/main-with-plugins')
+let FSWatcher = require('../lib/watcher')
 let Minimist = require('minimist')
-let FSWatcher = require('node-fswatcher')
 let Options = Minimist(process.argv.slice(2))
 let FS = require('fs')
 try {
   Options.blacklist = String(Options.blacklist || '').split(',').map(function(e) { return e.trim() }).filter(function(e) { return e && e !== 'true' })
   if(!Options['_'].length) throw new Error("Please specify a file/directory to watch")
-  let Watcher = new FSWatcher
-  Watcher.watch(Options['_'][0])
-  Watcher.on('update', function(Info){
-    FS.access(Info.Path, FS.R_OK, function(err){
-      if(!err)
-        Main.compileFile(Info.Path, Options).then(function(Output) {
-          Output = Output.contents
-          if(Options.Output){
-            FS.writeFileSync(Options.Output, Output)
-          }
-        }).catch(function(err){
-          console.log(err)
-        })
-    })
+  let Watcher = new FSWatcher(Options['_'][0])
+  Watcher.on('error', function(e){
+    console.log(e.stack)
   })
 } catch(err){
   console.log(err)
