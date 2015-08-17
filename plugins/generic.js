6bd7fc4ddb0dd8790c9226d94deb50339b2eface
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-17 12:02:35 -0600

    :bug: Fix compiler-include

--- plugins/generic.js
+++ plugins/generic.js
@@ -7,7 +7,9 @@ const Base = require('./base')
 class GenericPlugin extends Base {
   constructor() {
     super()
-    this.registerTag(['Compiler-Include'], function(name, value) {
+    this.registerTag(['Compiler-Include'], function(name, value, options) {
+      value = Path.resolve(options.internal.file.directory, value)
+      options.internal.imports.push(value)
       return new Promise(function(resolve, reject) {
         FS.readFile(value, function (err, data) {
           if (err) reject(err)
