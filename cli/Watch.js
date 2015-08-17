002c63c89db47cb0adf23100f66ef2ca31201053
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-17 11:59:40 -0600

    :new: Pass ignored option to watcher

--- cli/Watch.js
+++ cli/Watch.js
@@ -7,7 +7,7 @@ let FS = require('fs')
 try {
   Options.Blacklist = String(Options.Blacklist || '').split(',').map(function(e) { return e.trim() }).filter(function(e) { return e && e !== 'true' })
   if(!Options['_'].length) throw new Error("Please specify a file/directory to watch")
-  let Watcher = new FSWatcher(Options['_'][0])
+  let Watcher = new FSWatcher(Options['_'][0], Options.Ignored)
   Watcher.on('error', function(e){
     console.log(e.stack)
   })
