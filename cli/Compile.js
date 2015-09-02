16aa068f52653aea3a1e096db0bb68aa0ef80e49
steelbrain <aneesiqbalbhatti@gmail.com>
2015-09-02 06:56:23 -0600

    :new: Add support for compiling directories Fixes steelbrain/UCompiler#15

--- cli/Compile.js
+++ cli/Compile.js
@@ -7,15 +7,7 @@ let FS = require('fs')
 try {
   Options.blacklist = String(Options.blacklist || '').split(',').map(function(e) { return e.trim() }).filter(function(e) { return e && e !== 'true' })
   if(!Options['_'].length) throw new Error("Please specify a file to compile")
-  Main.compileFile(Options['_'][0], Options).then(function(Output) {
-    Options = Output.options
-    Output = Output.contents
-    if(Options.Output){
-      FS.writeFileSync(Options.Output, Output)
-    } else {
-      process.stdout.write(Output)
-    }
-  }).catch(function(err){
+  Main.compile(Options['_'][0], Options).then(Main.defaultOutput).catch(function(err){
     console.error(err)
   })
 } catch(err){
