574336d82b0903e26943c792cc769e6d315ecf89
steelbrain <aneesiqbalbhatti@gmail.com>
2015-09-15 08:43:43 -0600

    :art: Consume error handler in uc-compile

--- cli/Compile.js
+++ cli/Compile.js
@@ -7,9 +7,7 @@ let FS = require('fs')
 try {
   Options.blacklist = String(Options.blacklist || '').split(',').map(function(e) { return e.trim() }).filter(function(e) { return e && e !== 'true' })
   if(!Options['_'].length) throw new Error("Please specify a file to compile")
-  Main.compile(Options['_'][0], Options, Main.constructor.defaultOutput).catch(function(err){
-    console.error(err)
-  })
+  Main.compile(Options['_'][0], Options, Main.constructor.defaultOutput).catch(Main.constructor.defaultError)
 } catch(err){
   console.error(err)
 }
