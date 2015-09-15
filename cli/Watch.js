f03fa0e84c238c4745fd623cb73159ed718a67e7
steelbrain <aneesiqbalbhatti@gmail.com>
2015-09-15 10:22:54 -0600

    :bug: Fix a typo

--- cli/Watch.js
+++ cli/Watch.js
@@ -10,7 +10,7 @@ try {
   Options.Blacklist = String(Options.Blacklist || '').split(',').map(function(e) { return e.trim() }).filter(function(e) { return e && e !== 'true' })
   if(!Options['_'].length) throw new Error("Please specify a file/directory to watch")
   let Watcher = new FSWatcher(Options['_'][0], Options.Ignored)
-  Watcher.on('error', Main.constructor.defaultError)
+  Watcher.on('error', Main.defaultError)
 } catch(err){
   console.error(err)
 }
