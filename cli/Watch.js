4e4c59bbe798fbb08d1ec2f8d5782124ad051c2f
steelbrain <aneesiqbalbhatti@gmail.com>
2015-09-15 08:44:56 -0600

    :art: Consume error handler in uc-watch

--- cli/Watch.js
+++ cli/Watch.js
@@ -8,9 +8,7 @@ try {
   Options.Blacklist = String(Options.Blacklist || '').split(',').map(function(e) { return e.trim() }).filter(function(e) { return e && e !== 'true' })
   if(!Options['_'].length) throw new Error("Please specify a file/directory to watch")
   let Watcher = new FSWatcher(Options['_'][0], Options.Ignored)
-  Watcher.on('error', function(e){
-    console.error(e)
-  })
+  Watcher.on('error', Main.constructor.defaultError)
 } catch(err){
   console.error(err)
 }
