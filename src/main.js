004fc7f3650cffec3a804f40d4a11517186e1665
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-20 20:59:53 -0700

    :new: Make plugin architecture work

--- src/main.js
+++ src/main.js
@@ -13,16 +13,48 @@ export function compile(path, options = {
 }) {
   const config = getConfig(options.root)
   const files = scanFiles(path, {root: options.root, ignored: options.ignored})
-  const plugins = config.plugins.map(function(module) {
-    if (module.indexOf('ucompiler-plugin-') === 0) {
-      return require(module)
-    } else return require(`ucompiler-plugin-${module}`)
+  const plugins = {compilers: [], general: [], minifiers: []}
+
+  config.plugins.forEach(function(moduleName) {
+    const module = moduleName.indexOf('ucompiler-plugin-') === 0 ? require(moduleName) :require(`ucompiler-plugin-${moduleName}`)
+    if (module.compiler) {
+      plugins.compilers.push(module)
+    } else if (module.minifiers) {
+      plugins.minifiers.push(module)
+    } else {
+      plugins.general.push(module)
+    }
   })
 
-  files.forEach(function(file) {
-    // const contents = FS.readFileSync(file, {encoding: 'utf8'})
-    debug(`Compiling ${file}`)
-  })
+  return files.reduce(function(promise, file) {
+    return promise.then(function() {
+      const state = {sourceMap: null}
+      const contents = FS.readFileSync(file, {encoding: 'utf8'})
+
+      return plugins.compilers.reduce(function(promise, plugin) {
+        return promise.then(function(contents) {
+          return plugin.process({contents, file, config, state})
+        })
+      }, Promise.resolve(contents)).then(function(contents) {
+        return plugins.general.reduce(function(promise, plugin) {
+          return promise.then(function(contents) {
+            return plugin.process({contents, file, config, state})
+          })
+        }, Promise.resolve(contents))
+      }).then(function(contents) {
+        // TODO: Store if we have any source maps
+        return plugins.minifiers.reduce(function(promise, plugin) {
+          return promise.then(function(contents) {
+            return plugin.process({contents, file, config, state})
+          })
+        }, Promise.resolve(contents))
+      }).then(function(contents) {
+        debug(`Compiled ${file}`)
+      })
+    })
+  }, Promise.resolve())
 }
-//compile('**/*.js')
-//compile(__dirname)
+
+compile(__dirname).catch(function(e) {
+  console.log(e.stack)
+})
