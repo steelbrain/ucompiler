78407500e0835598ee0a309e4b5c5164ba56ae5b
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-20 20:15:13 -0700

    :new: Implement plugin import in main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 'use babel'
 
+import FS from 'fs'
 import {getConfig, scanFiles} from './helpers'
+import Debug from 'debug'
+
+Debug.enable('UCompiler:*')
+const debug = Debug('UCompiler:Main')
 
 export function compile(path, options = {
   root: process.cwd(),
-  ignored: ['{**/, }node_modules/**', '{**/, }.git/**']
+  ignored: ['{**/, }node_modules/**']
 }) {
   const config = getConfig(options.root)
   const files = scanFiles(path, {root: options.root, ignored: options.ignored})
-  console.log(config, files)
+  const plugins = config.plugins.map(function(module) {
+    if (module.indexOf('ucompiler-plugin-') === 0) {
+      return require(module)
+    } else return require(`ucompiler-plugin-${module}`)
+  })
+
+  files.forEach(function(file) {
+    // const contents = FS.readFileSync(file, {encoding: 'utf8'})
+    debug(`Compiling ${file}`)
+  })
 }
-compile('**/*.js')
+//compile('**/*.js')
+//compile(__dirname)
