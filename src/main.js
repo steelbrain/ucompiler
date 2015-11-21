90631efe9d07f964392ed9573cc04f406ab3c9e9
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-21 09:40:34 -0700

    :bug: Fix root detection in main

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 'use babel'
 
 import FS from 'fs'
-import {getConfig, scanFiles} from './helpers'
+import Path from 'path'
+import {getConfig, scanFiles, saveFile} from './helpers'
 import Debug from 'debug'
 
 Debug.enable('UCompiler:*')
 const debug = Debug('UCompiler:Main')
 
 export function compile(path, options = {
-  root: process.cwd(),
-  ignored: ['{**/, }node_modules/**']
+  ignored: ['{**/, }node_modules/**', '{**/, }.*']
 }) {
-  const config = getConfig(options.root)
-  const files = scanFiles(path, {root: options.root, ignored: options.ignored})
+  const config = getConfig(path)
+  const files = scanFiles(path, {root: config.root, ignored: options.ignored})
   const plugins = {compilers: [], general: [], minifiers: []}
 
   config.plugins.forEach(function(moduleName) {
@@ -26,8 +26,10 @@ export function compile(path, options = {
     }
   })
 
-  return files.reduce(function(promise, file) {
+  return files.reduce(function(promise, fileRelative) {
+    // TODO (For the future): Reverse source maps when changed
     return promise.then(function() {
+      const file = Path.join(config.root, fileRelative)
       const state = {sourceMap: null}
       const contents = FS.readFileSync(file, {encoding: 'utf8'})
 
@@ -42,19 +44,15 @@ export function compile(path, options = {
           })
         }, Promise.resolve(contents))
       }).then(function(contents) {
-        // TODO: Store if we have any source maps
         return plugins.minifiers.reduce(function(promise, plugin) {
           return promise.then(function(contents) {
             return plugin.process({contents, file, config, state})
           })
         }, Promise.resolve(contents))
       }).then(function(contents) {
-        debug(`Compiled ${file}`)
+        debug(`Processed ${file}`)
+        return saveFile({contents, file, config})
       })
     })
   }, Promise.resolve())
 }
-
-compile(__dirname).catch(function(e) {
-  console.log(e.stack)
-})
