72234afb020c637b6cc146f633744440698da521
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-24 08:21:40 -0700

    :new: Move default ignored to defaults

---
+++ src/defaults.js
@@ -0,0 +1,17 @@
+'use babel'
+
+export const DEFAULT_IGNORED = [
+  'node_modules',
+  'bower_components',
+  'coverage',
+  't{e, }mp',
+  '*.min.js',
+  '*.log',
+  'bundle.js',
+  'fixture{-*,}.{js,jsx}',
+  'spec',
+  'test{s, }',
+  'fixture{s, }',
+  'vendor',
+  'dist'
+]
