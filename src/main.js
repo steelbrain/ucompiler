a788741964caff44a1fa185f110fc44db3951b18
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-24 12:11:56 -0700

    :new: Use new helpers in main

--- src/main.js
+++ src/main.js
@@ -1,69 +1,27 @@
 'use babel'
 
-import Path from 'path'
-import Debug from 'debug'
-import {readFileSync} from 'fs'
-import {getConfig, getRules, scanFiles, saveFile, findRoot} from './helpers'
-import {DEFAULT_IGNORED} from './defaults'
+import {findRoot} from './helpers/find-root'
+import {getConfig} from './helpers/get-config'
+import {findFiles} from './helpers/find-files'
+import Util from 'util'
 
-const debugCompile = Debug('UCompiler:Compile')
-
-export function compile(path, options = {}, defaultRules = {}) {
-  options = Object.assign({
+export function compile(path, givenOptions = {}) {
+  const options = Object.assign({
     ignored: [],
     root: null,
-    defaultRoot: null
-  }, options)
-  options.ignored = options.ignored.concat(DEFAULT_IGNORED)
+    cwd: null,
+    rules: {}
+  }, givenOptions)
 
   const root = findRoot(path, options)
   const config = getConfig(root)
-  const files = scanFiles(path, {root: root, ignored: options.ignored})
-  const plugins = {compilers: [], general: [], minifiers: []}
+  const state = {
+    root: root,
+    config: config
+  }
+  const files = findFiles(path, options.ignored, state)
 
-  config.plugins.forEach(function(moduleName) {
-    const module = moduleName.indexOf('ucompiler-plugin-') === 0 ? require(moduleName) : require(`ucompiler-plugin-${moduleName}`)
-    if (module.compiler) {
-      plugins.compilers.push(module)
-    } else if (module.minifiers) {
-      plugins.minifiers.push(module)
-    } else {
-      plugins.general.push(module)
-    }
-  })
-
-  return Promise.all(files.map(function(relativePath) {
-    // TODO (For the future): Reverse source maps when changed
-    const file = Path.join(root, relativePath)
-    const state = {sourceMap: null, root, relativePath, imports: []}
-    const rules = getRules({path, state, config, defaultRules})
-    const contents = readFileSync(file, {encoding: 'utf8'})
-    const initialContents = contents
-
-    return plugins.compilers.reduce(function(promise, plugin) {
-      return promise.then(function(contents) {
-        return plugin.process({contents, file, config, state, rules})
-      })
-    }, Promise.resolve(contents)).then(function(contents) {
-      return plugins.general.reduce(function(promise, plugin) {
-        return promise.then(function(contents) {
-          return plugin.process({contents, file, config, state, rules})
-        })
-      }, Promise.resolve(contents))
-    }).then(function(contents) {
-      return plugins.minifiers.reduce(function(promise, plugin) {
-        return promise.then(function(contents) {
-          return plugin.process({contents, file, config, state, rules})
-        })
-      }, Promise.resolve(contents))
-    }).then(function(contents) {
-      if (initialContents !== contents) {
-        debugCompile(`Processed ${relativePath}`)
-        return saveFile({contents, file, config, state, rules, root})
-      } else {
-        debugCompile(`Ignored ${relativePath}`)
-        return contents
-      }
-    })
-  }))
+  console.log(files)
 }
+
+compile('src/helpers', {cwd: process.cwd()})
