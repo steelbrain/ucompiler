56c9ee0611428ec8db267d293d93d63f11b8021a
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-24 11:54:03 -0700

    :new: Use is-glob when finding root

--- src/helpers/find-root.js
+++ src/helpers/find-root.js
@@ -3,13 +3,14 @@
 import Path from 'path'
 import {getDir, findFile} from './common'
 import {CONFIG_FILE_NAME} from '../defaults'
+import isGlob from 'is-glob'
 
 export function findRoot(path, options) {
   if (options.root !== null) {
     return options.root
   }
 
-  const searchPath = getDir(path.indexOf('*') === -1 ? path : options.cwd)
+  const searchPath = getDir(isGlob(path) ? options.cwd : path)
   const configFile = findFile(searchPath, CONFIG_FILE_NAME)
 
   if (configFile === null) {
