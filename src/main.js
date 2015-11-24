a534b87bfa43d063f03c6356ec520c987bf471c7
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-24 07:59:15 -0700

    :art: Minor style nit

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ export function compile(path, options = {}, defaultRules = {}) {
   const plugins = {compilers: [], general: [], minifiers: []}
 
   config.plugins.forEach(function(moduleName) {
-    const module = moduleName.indexOf('ucompiler-plugin-') === 0 ? require(moduleName) :require(`ucompiler-plugin-${moduleName}`)
+    const module = moduleName.indexOf('ucompiler-plugin-') === 0 ? require(moduleName) : require(`ucompiler-plugin-${moduleName}`)
     if (module.compiler) {
       plugins.compilers.push(module)
     } else if (module.minifiers) {
