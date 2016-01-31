c7646e2295c3ad9c28f55c58ddf22bca62e0502b
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 14:34:02 -0700

    :new: Add a proper scan-files helper

---
+++ src/helpers/scan-files.js
@@ -0,0 +1,63 @@
+'use babel'
+
+/* @flow */
+
+import Path from 'path'
+import {readdir, stat, isExcluded, normalizePath} from './common'
+import {DEFAULT_IGNORED} from '../defaults'
+import type {Ucompiler$Config$Rule} from '../types'
+
+export async function scanFiles(rootDirectory: string, config: Ucompiler$Config$Rule): Promise<Array<string>> {
+  let files = []
+  let excluded = DEFAULT_IGNORED
+  if (config.exclude) {
+    excluded = excluded.concat(config.exclude)
+  }
+
+  for (const includeEntry of config.include) {
+    const allowedExtensions = new Set(includeEntry.extensions)
+    const includeDirectory = Path.join(rootDirectory, includeEntry.directory)
+    const searchDeep = typeof includeEntry.deep === 'undefined' ? true : includeEntry.deep
+    files = files.concat(await scanFilesInDirectory(
+      rootDirectory,
+      includeDirectory,
+      searchDeep,
+      allowedExtensions,
+      excluded
+    ))
+  }
+
+  return files
+}
+
+export async function scanFilesInDirectory(
+  rootDirectory: string,
+  directory: string,
+  deep: boolean,
+  extensions: Set<string>,
+  excluded: Array<string>
+): Promise<Array<string>> {
+  const contents = await readdir(directory)
+  let entries = []
+  for (const entry of contents) {
+    const entryPath = normalizePath(Path.join(directory, entry))
+    const entryRelativePath = normalizePath(Path.relative(rootDirectory, entryPath))
+    const entryStats = await stat(entryPath)
+
+    if (isExcluded([entryRelativePath, entryPath, entry], excluded)) {
+      continue
+    }
+
+    if (entryStats.isFile()) {
+
+      const extName = Path.extname(entry).slice(1)
+      if (extensions.has(extName)) {
+        entries.push(entryPath)
+      }
+
+    } else if (entryStats.isDirectory() && deep) {
+      entries = entries.concat(await scanFilesInDirectory(rootDirectory, entryPath, deep, extensions, excluded))
+    }
+  }
+  return entries
+}
