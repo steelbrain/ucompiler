79bb88cc05bbe72335ffe594069d89e451251ec3
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 15:49:01 -0700

    :new: Rewrite getPlugins helper

--- src/helpers/get-plugins.js
+++ src/helpers/get-plugins.js
@@ -1,31 +1,40 @@
 'use babel'
 
+/* @flow */
+
 import Path from 'path'
+import {exists} from './common'
+import type {Ucompiler$Plugins, UCompiler$Plugin, Ucompiler$Config$Rule} from '../types'
 
-export function getPlugins(root, config) {
-  const plugins = {compilers: [], general: [], minifiers: []}
+export async function getPlugins(rootDirectory: string, config: Ucompiler$Config$Rule): Promise<Ucompiler$Plugins> {
+  const plugins = {
+    compilers: [],
+    general: [],
+    minifiers: []
+  }
 
-  config.plugins.forEach(function(moduleName) {
-    moduleName = moduleName.indexOf('ucompiler-plugin-') === 0 ?
-      moduleName :
-      `ucompiler-plugin-${moduleName}`
-
-    let module = null
-
-    try {
-      module = require(Path.join(root, 'node_modules', moduleName))
-    } catch (_) {
-      module = require(moduleName)
-    }
-
-    if (module.compiler) {
-      plugins.compilers.push(module)
-    } else if (module.minifiers) {
-      plugins.minifiers.push(module)
+  for (const pluginName of config.plugins) {
+    const plugin = await getPlugin(rootDirectory, pluginName)
+    if (plugin.compiler) {
+      plugins.compilers.push(plugin)
+    } else if (plugin.minifier) {
+      plugins.minifiers.push(plugin)
     } else {
-      plugins.general.push(module)
+      plugins.general.push(plugin)
     }
-  })
+  }
 
   return plugins
+}
+
+export async function getPlugin(rootDirectory: string, pluginName: string): UCompiler$Plugin {
+  if (pluginName.indexOf('ucompiler-plugin-') !== 0) {
+    pluginName = 'ucompiler-plugin-' + pluginName
+  }
+  const pluginDirectory = Path.join(rootDirectory, 'node_modules', pluginName)
+  if (await exists(pluginDirectory)) {
+
+    // $FlowIgnore: Flow does not allow non-literal require paths
+    return require(pluginDirectory)
+  } else throw new Error(`'${pluginName}' not found in ${rootDirectory}`)
 }
