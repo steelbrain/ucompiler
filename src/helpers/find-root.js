c7d76aab9e98f564aea4bc43b0b0d5b6d16eaa99
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 14:11:31 -0700

    :art: Normalize root path

--- src/helpers/find-root.js
+++ src/helpers/find-root.js
@@ -3,12 +3,12 @@
 /* @flow */
 
 import Path from 'path'
-import {findCached} from './common'
+import {findCached, normalizePath} from './common'
 import {CONFIG_FILE_NAME} from '../defaults'
 
 export async function findRoot(directory: string): Promise<string> {
   const configFile = await findCached(directory, CONFIG_FILE_NAME)
   if (configFile) {
-    return Path.dirname(configFile)
+    return normalizePath(Path.dirname(configFile))
   } else throw new Error('No configuration found file')
 }
