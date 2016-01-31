c09183fd27bcbebc6204472282c90b95143c08fb
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 13:22:37 -0700

    :new: Rewrite find-root helper

--- src/helpers/find-root.js
+++ src/helpers/find-root.js
@@ -1,40 +1,14 @@
 'use babel'
 
+/* @flow */
+
 import Path from 'path'
-import {getDir, findFile, normalizePath} from './common'
+import {findCached} from './common'
 import {CONFIG_FILE_NAME} from '../defaults'
-import isGlob from 'is-glob'
 
-const ERROR_MESSAGE = 'Either of options.cwd or options.root is required'
-
-export function findRoot(path, options) {
-  if (options.root !== null) {
-    return normalizePath(options.root)
-  } else if (path === null) {
-    if (options.cwd === null) {
-      throw new Error(ERROR_MESSAGE)
-    } else return normalizePath(options.cwd)
-  }
-
-  const pathIsGlob = isGlob(path)
-  const pathIsAbsolute = !pathIsGlob && Path.isAbsolute(path)
-
-  if (!pathIsAbsolute && options.cwd === null) {
-    throw new Error(ERROR_MESSAGE)
-  }
-
-  const searchPath = getDir(
-    pathIsGlob ?
-      options.cwd :
-      pathIsAbsolute ?
-        path :
-        Path.join(options.cwd, path)
-  )
-  const configFile = findFile(searchPath, CONFIG_FILE_NAME)
-
-  if (configFile === null) {
-    return normalizePath(searchPath)
-  } else {
-    return normalizePath(Path.dirname(configFile))
-  }
+export function findRoot(directory: string): string {
+  const configFile = findCached(directory, CONFIG_FILE_NAME)
+  if (configFile !== null) {
+    return Path.dirname(String(configFile))
+  } else return directory
 }
