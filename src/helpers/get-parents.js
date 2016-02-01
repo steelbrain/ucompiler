722de47f725e27dad2d5c7939b8311b6fc41a26f
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-01 13:06:36 -0700

    :new: Add a proper getParents helper

--- src/helpers/get-parents.js
+++ src/helpers/get-parents.js
@@ -2,8 +2,18 @@
 
 /* @flow */
 
-export function getParents() {
-  const parents = []
+export function getParents(files: Map<string, Array<string>>): Map<string, Array<string>> {
+  const parents = new Map()
+
+  for (const [filePath, imports] of files) {
+    for (const entry of imports) {
+      let entryParents = parents.get(entry)
+      if (!entryParents) {
+        parents.set(entry, entryParents = [])
+      }
+      entryParents.push(filePath)
+    }
+  }
 
   return parents
 }
