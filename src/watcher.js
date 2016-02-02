488cf529699a14a4de803acb95d7f0a987e10383
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-02 15:47:41 -0700

    :new: Implement a basic watcher in watcher class

--- src/watcher.js
+++ src/watcher.js
@@ -2,11 +2,12 @@
 
 /* @flow */
 
+import Path from 'path'
 import debug from 'debug'
 import Chokidar from 'chokidar'
 import {CompositeDisposable, Disposable} from 'sb-event-kit'
 import {findRoot} from './helpers/find-root'
-import {getConfig} from './helpers/get-config'
+import {getConfig, getConfigRule} from './helpers/get-config'
 import type {UCompiler$Options, Ucompiler$Config} from './types'
 import type {Chokidar$Watcher, EventKit$CompositeDisposable} from './types-external'
 
@@ -16,6 +17,7 @@ export class Watcher {
   options: UCompiler$Options;
   ruleNames: Array<string>;
   subscriptions: EventKit$CompositeDisposable;
+  watcherReady: boolean;
 
   constructor(
     rootDirectory:string,
@@ -26,14 +28,34 @@ export class Watcher {
     this.rootDirectory = rootDirectory
     this.config = config
     this.options = options
-    this.ruleNames = ruleNames
+    this.ruleNames = ruleNames.length ? ruleNames : [config.defaultRule]
     this.subscriptions = new CompositeDisposable()
+    this.watcherReady = false
   }
   activate() {
     const watcher = Chokidar.watch([])
-    watcher.on('ready', function() {
+    const watcherCallback = filePath => {
+      console.log('watcher')
+    }
+
+    this.ruleNames.forEach(ruleName => {
+      const config = getConfigRule(this.config, ruleName)
+      for (const rule of config.include) {
+        watcher.add(Path.join(this.rootDirectory, rule.directory))
+      }
+    })
+
+    watcher.on('add', watcherCallback)
+    watcher.on('change', watcherCallback)
+    watcher.on('ready', () => {
+      this.watcherReady = true
       console.log('ready')
     })
+
+    this.subscriptions.add(new Disposable(function() {
+      this.watcherReady = false
+      watcher.close()
+    }))
   }
   dispose() {
 
@@ -45,7 +67,7 @@ export class Watcher {
     ruleNames: Array<string> = []
   ): Promise<Watcher> {
     const rootDirectory = await findRoot(directory)
-    const {global: config} = await getConfig(rootDirectory)
+    const config = await getConfig(rootDirectory, null)
 
     return new Watcher(rootDirectory, config, options, ruleNames)
   }
