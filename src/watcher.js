113b6eb287211215014ad51d35041ca65be7ea2f
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-02 18:12:06 -0700

    :new: Add a basic handle change and activation logic to watcher

--- src/watcher.js
+++ src/watcher.js
@@ -8,7 +8,10 @@ import Chokidar from 'chokidar'
 import {CompositeDisposable, Disposable} from 'sb-event-kit'
 import {findRoot} from './helpers/find-root'
 import {getConfig, getConfigRule} from './helpers/get-config'
-import type {UCompiler$Options, Ucompiler$Config} from './types'
+import {validateNode} from './helpers/scan-files'
+import {compileFile} from './helpers/compile-file'
+import type {Stats} from 'fs'
+import type {UCompiler$Options, Ucompiler$Config, Ucompiler$Compile$Result} from './types'
 import type {Chokidar$Watcher, EventKit$CompositeDisposable} from './types-external'
 
 export class Watcher {
@@ -17,7 +20,13 @@ export class Watcher {
   options: UCompiler$Options;
   ruleNames: Array<string>;
   subscriptions: EventKit$CompositeDisposable;
+  imports: Map<string, Array<string>>;
+  locks: Set<string>;
   watcherReady: boolean;
+  activation: {
+    promises: Array<Promise>,
+    results: Map<string, Ucompiler$Compile$Result>
+  };
 
   constructor(
     rootDirectory:string,
@@ -30,23 +39,29 @@ export class Watcher {
     this.options = options
     this.ruleNames = ruleNames.length ? ruleNames : [config.defaultRule]
     this.subscriptions = new CompositeDisposable()
+    this.imports = new Map()
+    this.locks = new Set()
     this.watcherReady = false
+    this.activation = {
+      promises: [],
+      results: new Map()
+    }
   }
   activate() {
     const watcher = Chokidar.watch([])
     const watcherCallback = filePath => {
-      console.log('watcher')
+      console.log('watcher', filePath)
     }
 
-    this.ruleNames.forEach(ruleName => {
+    for (const ruleName of this.ruleNames) {
       const config = getConfigRule(this.config, ruleName)
       for (const rule of config.include) {
         watcher.add(Path.join(this.rootDirectory, rule.directory))
       }
-    })
+    }
 
-    watcher.on('add', watcherCallback)
-    watcher.on('change', watcherCallback)
+    watcher.on('add', (filePath, stats) => this.handleChange(filePath, stats))
+    watcher.on('change', (filePath, stats) => this.handleChange(filePath, stats))
     watcher.on('ready', () => {
       this.watcherReady = true
       console.log('ready')
@@ -57,8 +72,30 @@ export class Watcher {
       watcher.close()
     }))
   }
+  async handleChange(filePath: string, stats: Stats): Promise {
+    for (const ruleName of this.ruleNames) {
+      const config = getConfigRule(this.config, ruleName)
+      for (const rule of config.include) {
+        const ruleDirectory = Path.join(this.rootDirectory, rule.directory)
+        if (filePath.indexOf(ruleDirectory) === 0 && validateNode(
+          this.rootDirectory, filePath, config.exclude || [], rule.extensions, stats, false
+        )) {
+          this.locks.add(filePath)
+          const result = await compileFile(this.rootDirectory, filePath, config)
+          this.locks.delete(filePath)
+
+          console.log(result)
+          break
+        }
+      }
+    }
+  }
   dispose() {
     this.subscriptions.dispose()
+    this.imports.clear()
+    this.locks.clear()
+    this.activation.promises = []
+    this.activation.results.clear()
   }
 
   static async create(
