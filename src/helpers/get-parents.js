5f3aa2a9f36e1b4f1f5904f4e18511fefd5ef809
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-02 19:38:40 -0700

    :bug: Fix a bug with get-parents Where it will not work at all

--- src/helpers/get-parents.js
+++ src/helpers/get-parents.js
@@ -5,10 +5,10 @@
 export function getParents(importsMap: Map<string, Array<string>>, filePath: string): Array<string> {
   const parents = []
 
-  for (const [filePath, imports] of importsMap) {
+  for (const [parentFile, imports] of importsMap) {
     for (const entry of imports) {
       if (entry === filePath) {
-        parents.push(filePath)
+        parents.push(parentFile)
         break
       }
     }
